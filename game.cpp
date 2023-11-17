d866d710d00242c0ac5237ccece71f753ab0af9c
Luisardo0 <darhead0@gmail.com>
2023-11-17 16:12:46 -0600

    Se crea el proyecto del juego

---
+++ game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
+void escojer_palabra(){
+    /*
+    listaPalabras (arreglo): lista de palabras (strings)
+    Retorna una palabra aleatoria de la lista de palabras
+    */
+}
