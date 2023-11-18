fb2685b0661c879e5e411e0c4d47714feef94562
Luisardo0 <darhead0@gmail.com>
2023-11-17 18:30:08 -0600

    Se implementa la función para escoger una palabra de un vector de strings para el hangman

--- game.cpp
+++ game.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <stdlib.h>
 using namespace std;
+
+
+string choose_palabra(const vector<string>& listaPalabras){
+    /*
+    listaPalabras (vector de strings): lista de palabras (strings)
+    Devuelve una palabra aleatoria de la lista de palabras
+    */
+    srand(static_cast<unsigned int>(time(0)));
+    int randomIndex = rand() % listaPalabras.size();
+    return listaPalabras[randomIndex];
+}
 
 int main()
 {
-    
+    vector<string> palabras ={"gato","perro","tarde","bufalo","conejo"};
+    string word = choose_palabra(palabras);
+    cout<<"Palabra aleatoria: "<<word;
     return 0;
-}
-void escojer_palabra(){
-    /*
-    listaPalabras (arreglo): lista de palabras (strings)
-    Retorna una palabra aleatoria de la lista de palabras
-    */
 }
