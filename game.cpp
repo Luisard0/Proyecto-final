239d32237c54987bd29b5fec0647ba16e57896aa
Luisardo0 <darhead0@gmail.com>
2023-11-17 18:57:30 -0600

    Se implementa la funcion palabraAdivinada para saber si la palabra ha sido adivinada, para el hangman

--- game.cpp
+++ game.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 #include <ctime>
 #include <stdlib.h>
+#include <algorithm>
 using namespace std;
 
-
+//Escoge una palbra de un vector de palabras
 string choose_palabra(const vector<string>& listaPalabras){
     /*
     listaPalabras (vector de strings): lista de palabras (strings)
@@ -15,10 +16,23 @@ string choose_palabra(const vector<string>& listaPalabras){
     return listaPalabras[randomIndex];
 }
 
+//Reviza si la palabra ha sido adivinada
+bool palabraAdivinada(const string& palabra_secreta, const vector<char>& letras_adivinadas){
+    vector<char> lista;
+        for (char letter : palabra_secreta) {
+        if (find(letras_adivinadas.begin(), letras_adivinadas.end(), letter) != letras_adivinadas.end()) {
+            lista.push_back(letter);
+        }
+    }
+    return palabra_secreta.length()==lista.size();
+}
+
 int main()
 {
-    vector<string> palabras ={"gato","perro","tarde","bufalo","conejo"};
-    string word = choose_palabra(palabras);
-    cout<<"Palabra aleatoria: "<<word;
+    string palabrasecreta = "python";
+    vector<char> letrasAdivinadas={'p','y','t','h','o','n'};
+    bool adivinada = palabraAdivinada(palabrasecreta,letrasAdivinadas);
+
+    cout<<"Palabra adivinada? "<<(adivinada ? "Si":"No")<<endl;
     return 0;
 }
