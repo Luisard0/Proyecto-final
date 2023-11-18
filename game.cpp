f6fa1f323088ed46201fdc6cd7dd705e22785eeb
Luisardo0 <darhead0@gmail.com>
2023-11-17 19:05:02 -0600

    Documentación de palabraAdivinada

--- game.cpp
+++ game.cpp
@@ -18,12 +18,20 @@ string choose_palabra(const vector<string>& listaPalabras){
 
 //Reviza si la palabra ha sido adivinada
 bool palabraAdivinada(const string& palabra_secreta, const vector<char>& letras_adivinadas){
+    /*
+    palabra secreta: string, la palabra que el usuario está adivinando; 
+     se asume que todas las letras son minusculas
+    letras_adivinadas: vector (de caracteres), letras que ya han sido adivinadas;
+     se asume que todas las letras son minusculas
+    Devuelve un booleano, Verdadero si todas las letras de palabra_secreta están en letras_adivinadas,
+    de otra manera Falso.
+    */
     vector<char> lista;
         for (char letter : palabra_secreta) {
         if (find(letras_adivinadas.begin(), letras_adivinadas.end(), letter) != letras_adivinadas.end()) {
             lista.push_back(letter);
         }
-    }
+        }
     return palabra_secreta.length()==lista.size();
 }
 
